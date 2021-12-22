ba4413ded256d2e5a9fd68f9fd490f0c0daaf446
wilmermad <wilmermad@hotmail.com>
2021-12-21 21:29:37 -0500

    agregar uri BD

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = process.env.BD_URI;
+const URI = process.env.BD_URI || 'mongodb://wmadera:wmadera@cluster0-shard-00-00.ukhjf.mongodb.net:27017,cluster0-shard-00-01.ukhjf.mongodb.net:27017,cluster0-shard-00-02.ukhjf.mongodb.net:27017/recogida_paquetes?ssl=true&replicaSet=atlas-ci2jgb-shard-0&authSource=admin&retryWrites=true&w=majority';
 
 mongoose.connect(URI);
 
